e370feccbf0d6ae39c32f9186799ec7d973d7700
Kaitlyn W <kaitlnl27@gmail.com>
2023-07-18 17:50:54 -0400

    Update register blocks

--- holding_register.vhd
+++ holding_register.vhd
@@ -5,10 +5,10 @@ use ieee.std_logic_1164.all;
 entity holding_register is port (
 
 			clk					: in std_logic;
-			reset					: in std_logic;
-			register_clr			: in std_logic;
+			reset				: in std_logic;
+			register_clr		: in std_logic;
 			din					: in std_logic;
-			dout					: out std_logic
+			dout				: out std_logic
   );
  end holding_register;
  
@@ -18,7 +18,12 @@ entity holding_register is port (
 
 
 BEGIN
+	reg: process (clk)
+		begin
 
-
-
+		if(rising_edge(clk)) then
+			dout <= (din or dout) and (not (register_clear or reset));
+		end if;
+		
+	end process
 end;
--- synchronizer.vhd
+++ synchronizer.vhd
@@ -4,20 +4,33 @@ use ieee.std_logic_1164.all;
 
 entity synchronizer is port (
 
-			clk					: in std_logic;
-			reset					: in std_logic;
-			din					: in std_logic;
-			dout					: out std_logic
-  );
- end synchronizer;
+			clk					: in std_logic;  
+			reset				: in std_logic; 
+			din					: in std_logic; 
+			dout				: out std_logic 
+);
+end synchronizer; 
  
  
 architecture circuit of synchronizer is
 
-	Signal sreg				: std_logic_vector(1 downto 0);
+Signal sreg 		 : std_logic_vector(1 downto 0);  
 
 BEGIN
+	-- 2 bit shift register
+	reg : process (reset, clk)
+		begin
+			
+		if (reset='1') then
+			sreg <= "00";
 
+		else
+			if	(rising_edge(clk)) then
+				sreg <= sreg(0) & din;
+			end if;
+		end if;
 
+		dout <= sreg(1);
 
+	end process;
 end;
